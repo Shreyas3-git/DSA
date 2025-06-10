e80f7b9008f17080d5c155ddf352c91ba222c4b2
Shreyas Shinde <83245241+Shreyas3-git@users.noreply.github.com>
2025-06-10 21:19:08 +0530

    solve array revision question

--- src/main/java/com/example/dsa_practice2025/array/CyclicRotation.java
+++ src/main/java/com/example/dsa_practice2025/array/CyclicRotation.java
@@ -40,18 +40,10 @@ public class CyclicRotation
     private static void reverseArr(int[] arr) {
         int n = arr.length;
         int temp = arr[n-1];
-        //  0 1 2 3 4
-        // [1,2,3,4,5]
-        // [1,2,3,4,4]
-        // [1,2,3,3,4]             i=3
-        // [1,2,2,3,4]             i = 2
-        // [1,1,2,3,4]             i = 1
-        // [5,1,2,3,4]
         for(int i = n-1; i > 0; i--) {
             arr[i] = arr[i-1];
         }
         arr[0] = temp;
-        return;
     }
 
 }
